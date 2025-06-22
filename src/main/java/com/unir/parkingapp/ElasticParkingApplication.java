70e25c693b1ff951118e3f9c05290cbc10d2e245
Moi6-23 <ariasmoises993@gmail.com>
2025-06-22 16:35:22 -0500

    Initial commit

---
+++ src/main/java/com/unir/parkingapp/ElasticParkingApplication.java
@@ -0,0 +1,14 @@
+package com.unir.parkingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ElasticParkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticParkingApplication.class, args);
+	}
+
+}
